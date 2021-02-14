f560aca7beb6bdd3bf242fff635acf458e79ed6b
martinesidens <martines.idens@gmail.com>
2021-02-14 16:08:30 +0300

    14.02.2021 16:10

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ const maxX = 35.70000;
 const minY = 139.70000;
 const maxY = 139.80000;
 
+const minNum = 1;
+const maxNum = 8;
+
+let titleAdvt = '';
+let descriptionAdvt = '';
+
 const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
@@ -34,34 +40,29 @@ function getLocation() {
   } 
 }
 
-function getAuthor (minNumber, maxNumber) {
-  return {
-    avatar: 'img/avatars/user0' + getRandomFloat(minNumber, maxNumber) + '.png',
-  }
-}
-
-function getOffer () {   
-  return {
-    title: 'Сдается квартира на улице Хошимина',
-    address: address.x + ',' + address.y,
-    price: Math.abs(2500),
-    type: type[getRandomInt(0, type.length - 1)],
-    rooms: Math.abs(2),
-    guests: Math.abs(4),
-    checkin: atTime[getRandomInt(0, atTime.length - 1)],
-    checkout: atTime[getRandomInt(0, atTime.length - 1)],
-    features: getRandomArray(features),
-    description: 'Мебель, посуда и отличный вид на соседние трущобы.',
-    photos: getRandomArray(photos),
-  }
-}
-
 function createAdvertisement() {
   const address = getLocation();
   return {
-    autor: getAuthor(),
-    offer: getOffer(),
-    location: address,
+    autor: {
+      avatar: 'img/avatars/user0' + getRandomInt(minNum, maxNum) + '.png',
+    },
+    offer: {
+      title: titleAdvt,
+      address: address.x + ',' + address.y,
+      price: Math.abs(2500),
+      type: type[getRandomInt(0, type.length - 1)],
+      rooms: Math.abs(2),
+      guests: Math.abs(4),
+      checkin: atTime[getRandomInt(0, atTime.length - 1)],
+      checkout: atTime[getRandomInt(0, atTime.length - 1)],
+      features: getRandomArray(features),
+      description: descriptionAdvt,
+      photos: getRandomArray(photos),
+    },
+    location: {
+      x: address.x,
+      y: address.y,
+    },
   }
 }
 
