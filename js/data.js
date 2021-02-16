96a81aa1840d5197a60258660ff8dec5ac7e2b48
martinesidens <martines.idens@gmail.com>
2021-02-16 17:01:11 +0300

    16.02.2021 17:01

--- js/data.js
+++ js/data.js
@@ -1,31 +1,31 @@
 import {getRandomArray, getRandomFloat, getRandomInt} from './util.js';
 
-let type = ['palace','flat','house','bungalow'];
-let atTime = ['12:00', '13:00', '14:00'];
+let TYPE = ['palace','flat','house','bungalow'];
+let AT_TIME = ['12:00', '13:00', '14:00'];
 
-const minX = 35.65000;
-const maxX = 35.70000;
-const minY = 139.70000;
-const maxY = 139.80000;
+const MIN_X = 35.65000;
+const MAX_X = 35.70000;
+const MIN_Y = 139.70000;
+const MAX_Y = 139.80000;
 
-const minNum = 1;
-const maxNum = 8;
+const MIN_NUM = 1;
+const MAX_NUM = 8;
 
-let minRooms = 2;
-let maxRooms = 5;
-let minGuests = 4;
-let maxGuests = 10;
+const MIN_ROOMS = 2;
+const MAX_ROOMS = 5;
+let MIN_GUESTS = 4;
+let MAX_GUESTS = 10;
 
 let titleAdvt = '';
 let descriptionAdvt = '';
 
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 function getLocation() { 
   return { 
-    x: getRandomFloat(minX, maxX, 5),
-    y: getRandomFloat(minY, maxY, 5),
+    x: getRandomFloat(MIN_X, MAX_X, 5),
+    y: getRandomFloat(MIN_Y, MAX_Y, 5),
   } 
 }
 
@@ -33,20 +33,20 @@ function createAdvertisement() {
   const address = getLocation();
   return {
     autor: {
-      avatar: 'img/avatars/user0' + getRandomInt(minNum, maxNum) + '.png',
+      avatar: 'img/avatars/user0' + getRandomInt(MIN_NUM, MAX_NUM) + '.png',
     },
     offer: {
       title: titleAdvt,
       address: address.x + ',' + address.y,
-      price: Math.abs(2500),
-      type: type[getRandomInt(0, type.length - 1)],
-      rooms: Math.abs(getRandomInt(minRooms, maxRooms)),
-      guests: Math.abs(getRandomInt(minGuests, maxGuests)),
-      checkin: atTime[getRandomInt(0, atTime.length - 1)],
-      checkout: atTime[getRandomInt(0, atTime.length - 1)],
+      price: Math.abs(getRandomInt(MIN_PRICE, MAX_PRICE)),
+      type: TYPE[getRandomInt(0, TYPE.length - 1)],
+      rooms: Math.abs(getRandomInt(MIN_ROOMS, MAX_ROOMS)),
+      guests: Math.abs(getRandomInt(MIN_GUESTS, MAX_GUESTS)),
+      checkin: AT_TIME[getRandomInt(0, AT_TIME.length - 1)],
+      checkout: AT_TIME[getRandomInt(0, AT_TIME.length - 1)],
       features: getRandomArray(features),
       description: descriptionAdvt,
-      photos: getRandomArray(photos),
+      photos: getRandomArray(PHOTOS),
     },
     x: address.x,
     location: {
@@ -57,8 +57,8 @@ function createAdvertisement() {
 
 function getAdvt() {
   return [
-    new Array(10).fill('').map(() => createAdvertisement())   
+    Array(10).fill('').map(() => createAdvertisement())   
   ]
 }
 
-export {getAdvt, createAdvertisement};
+export {getAdvt};
