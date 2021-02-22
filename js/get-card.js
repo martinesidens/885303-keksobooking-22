8610be7ec050db8cde60dbb08f4820131b7944d4
martinesidens <martines.idens@gmail.com>
2021-02-22 15:24:45 +0300

    22.02.2021 15:25

--- js/get-card.js
+++ js/get-card.js
@@ -2,22 +2,19 @@ import {getAdvt} from './data.js';
 
 const advt = getAdvt();
 
-console.log(advt)
+
+
 const mapCanvas = document.querySelector('#map-canvas');
 const templateCard = document.querySelector('#card').content;
 
-advt.forEach((advt) => {
+advt.forEach(() => {
   const advtElement = templateCard.cloneNode(true);
   advtElement.querySelector('.popup__title').textContent;
   mapCanvas.appendChild(advtElement);
+  console.log(advt);
 });
 
-
-
 function getCard (advt) {
-  const title = templateCard.querySelector('.popup__title').textContent;
-
-  // console.log(title);
 }
 
 
