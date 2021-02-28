e4382d360bed55ebc49ea969f586b16da8f73b10
martinesidens <martines.idens@gmail.com>
2021-02-28 17:10:24 +0300

    28.02.2021 17:15

--- js/data.js
+++ js/data.js
@@ -1,29 +1,5 @@
 import {getRandomArray, getRandomFloat, getRandomInt} from './util.js';
-
-const TYPE = ['palace','flat','house','bungalow'];
-const AT_TIME = ['12:00', '13:00', '14:00'];
-
-const MIN_X = 35.65000;
-const MAX_X = 35.70000;
-const MIN_Y = 139.70000;
-const MAX_Y = 139.80000;
-
-const MIN_NUM = 1;
-const MAX_NUM = 8;
-
-const MIN_ROOMS = 2;
-const MAX_ROOMS = 5;
-const MIN_GUESTS = 4;
-const MAX_GUESTS = 10;
-
-const MIN_PRICE = 1500;
-const MAX_PRICE = 10500;
-
-let titleAdvt = 'Тестовый заголовок';
-let descriptionAdvt = 'Тестовое описание';
-
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+import {TYPE, AT_TIME, MIN_X, MAX_X, MIN_Y, MAX_Y, MIN_NUM, MAX_NUM, MIN_ROOMS, MAX_ROOMS, MIN_GUESTS, MAX_GUESTS, MIN_PRICE, MAX_PRICE, titleAdvt, descriptionAdvt, FEATURES, PHOTOS} from './variables.js';
 
 function getLocation() {
   return {
