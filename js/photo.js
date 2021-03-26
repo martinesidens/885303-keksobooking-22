901ad42f68a7804ab6e5be1009efab3e3898ca0d
martinesidens <martines.idens@gmail.com>
2021-03-26 12:32:50 +0300

    26/03/2021 12:35

--- js/photo.js
+++ js/photo.js
@@ -2,30 +2,27 @@ const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const fileChoseElement = document.querySelector('.ad-form-header__input');
 const preview = document.querySelector('.ad-form-header__preview img');
-const fileChosePhoto = document.querySelector('.ad-form__input');
 
 
-function setAvatarPhoto () {
-    fileChoseElement.addEventListener('change', () => {
-      const file = fileChoseElement.files[0];
-      const fileName = file.name.toLowerCase();
-      console.log(file);
-    
-      const matches = FILE_TYPES.some((element) => {
-        return fileName.endsWith(element);
-      });
-    
-      if (matches) {
-        const reader = new FileReader();
-    
-        reader.addEventListener('load', () => {
-            preview.src = reader.result;
-          });
-          reader.readAsDataURL(file);
-      }
-    
+function setAvatarPhoto() {
+  fileChoseElement.addEventListener('change', () => {
+    const file = fileChoseElement.files[0];
+    const fileName = file.name.toLowerCase();
+
+    const matches = FILE_TYPES.some((element) => {
+      return fileName.endsWith(element);
     });
+
+    if (matches) {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', () => {
+        preview.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  });
 }
 
-export {setAvatarPhoto};
+export { setAvatarPhoto };
 
