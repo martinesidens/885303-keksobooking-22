79507be994e4db75d89124afda3ab9b56d80e7ca
martinesidens <martines.idens@gmail.com>
2021-03-26 12:29:14 +0300

    26/03/2021 12:30

--- js/photo.js
+++ js/photo.js
@@ -1,6 +1,31 @@
-// const fileChoseElement = document.querySelector('.ad-form__field');
-// const preview = document.querySelector('.ad-form-header__preview');
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
-// fileChoseElement.addEventListener('change', () => {
+const fileChoseElement = document.querySelector('.ad-form-header__input');
+const preview = document.querySelector('.ad-form-header__preview img');
+const fileChosePhoto = document.querySelector('.ad-form__input');
 
-// });
+
+function setAvatarPhoto () {
+    fileChoseElement.addEventListener('change', () => {
+      const file = fileChoseElement.files[0];
+      const fileName = file.name.toLowerCase();
+      console.log(file);
+    
+      const matches = FILE_TYPES.some((element) => {
+        return fileName.endsWith(element);
+      });
+    
+      if (matches) {
+        const reader = new FileReader();
+    
+        reader.addEventListener('load', () => {
+            preview.src = reader.result;
+          });
+          reader.readAsDataURL(file);
+      }
+    
+    });
+}
+
+export {setAvatarPhoto};
+
